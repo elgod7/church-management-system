610166f7f0858836926f5d4390154dbb7bb73294
Elias Godson <eng.eliasgodson@gmail.com>
2023-11-12 11:14:12 +0300

    minor change

--- app/(app)/about/page.tsx
+++ app/(app)/about/page.tsx
@@ -5,8 +5,8 @@ import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
+    <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-3">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Beautifully designed components <br className="hidden sm:inline" />
           built with Radix UI and Tailwind CSS.
